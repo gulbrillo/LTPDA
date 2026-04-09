d61b453476a8aa31484b9f2ffeffb888eb5e7d0e
gulbrillo <simon.barke@gmail.com>
2026-04-08 21:00:32 -0400

    trying again

--- repository/frontend/app/app.vue
+++ repository/frontend/app/app.vue
@@ -1,11 +1,5 @@
-<script setup lang="ts">
-const route = useRoute()
-</script>
-
 <template>
   <NuxtLayout>
-    <Transition name="page" mode="out-in">
-      <NuxtPage :key="route.path" />
-    </Transition>
+    <NuxtPage :transition="{ name: 'page', mode: 'out-in' }" />
   </NuxtLayout>
 </template>
